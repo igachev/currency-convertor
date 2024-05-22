c40d1db557b374a88d8c7489d6902c084cc6baf7
igachev <ivophoenix1135@gmail.com>
2024-05-22 14:15:29 +0300

    make a get request and calculate result

--- CurrencyConversion.js
+++ CurrencyConversion.js
@@ -5,20 +5,50 @@ const apiKey = config.api_key
 
 function currencyConversion() {
     const givenDate = process.argv[2]
-    
+    let amount = 0;
+    let baseCurrency = "";
+    let targetCurrency = "";
+
     let rl = readline.createInterface(process.stdin,process.stdout)
     
+    let enterData = function() {
 
-    let enterAmount = function() {
-        rl.question("",function(amount) {
-            console.log(typeof amount)
+        rl.question("",function(selectedAmount) {
+            amount = selectedAmount
+            console.log("amount:" + selectedAmount)
+
+            rl.question("",function(selectedBaseCurrency) {
+                baseCurrency = selectedBaseCurrency.toUpperCase()
+                console.log("base currency:" + selectedBaseCurrency)
+
+                rl.question("",function(selectedTargetCurrency) {
+                    targetCurrency = selectedTargetCurrency.toUpperCase()
+                    console.log("target currency:" + selectedTargetCurrency)
+
+                    const url = `https://api.fastforex.io/historical?date=${givenDate}&from=${baseCurrency}&api_key=${apiKey}`
+                    fetch(url)
+                    .then((res) => res.json())
+                    .then((data) => {
+                        let currentExchangeRate = Number(data.results[targetCurrency])
+                        let calculation = amount * currentExchangeRate
+                        let result = `${amount} ${baseCurrency} is ${calculation.toFixed(2)} ${targetCurrency}`
+                        console.log(result)
+                    })
+                    
+                    })
+            })
+            
+                
         })
+     
+
+   
     }
     
-    enterAmount()
+    enterData()
+   
 
-    const url = `https://api.fastforex.io/fetch-all?api_key=${apiKey}`
-  //  console.log(url)
+   
 }
 
 currencyConversion()
