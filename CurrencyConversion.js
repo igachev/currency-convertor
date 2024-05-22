fe95637db245a54482ff1bd8c15257d3c98a2a19
igachev <ivophoenix1135@gmail.com>
2024-05-22 16:50:31 +0300

    added required validations for amount,baseCurrency,targetCurrency

--- CurrencyConversion.js
+++ CurrencyConversion.js
@@ -4,6 +4,9 @@ let readline = require('readline')
 fs = require('fs');
 const apiKey = config.api_key
 
+
+
+
 function currencyConversion() {
     const givenDate = process.argv[2]
     let amount = 0;
@@ -12,17 +15,29 @@ function currencyConversion() {
 
     let rl = readline.createInterface(process.stdin,process.stdout)
 
-    
+    const urlCurrencies = `https://api.fastforex.io/currencies?api_key=${apiKey}`;
     
     let enterData = function() {
 
         rl.question("",function(selectedAmount) {
+
+            if(selectedAmount.includes(".")) {
+               let splitAmount = selectedAmount.split('.')
+                if(splitAmount[1].length > 2) {
+                    console.log("Please enter a valid amount")
+                    enterData()
+                }
+            }
 
             if(selectedAmount === 'END') {
                return rl.close()
             }
 
             amount = selectedAmount
+            if(isNaN(amount)) {
+                console.log("Please enter a valid amount")
+                enterData()
+            }
             console.log("amount:" + selectedAmount)
 
             rl.question("",function(selectedBaseCurrency) {
@@ -33,47 +48,75 @@ function currencyConversion() {
 
                 baseCurrency = selectedBaseCurrency.toUpperCase()
                 console.log("base currency:" + selectedBaseCurrency)
+                // validation
+                fetch(urlCurrencies)
+                .then((res) => res.json())
+                .then((data) => {
+                    let validCurrencies = data.currencies;
+                    if(baseCurrency in validCurrencies) {
+                        rl.question("",function(selectedTargetCurrency) {
 
-                rl.question("",function(selectedTargetCurrency) {
-
-                    if(selectedTargetCurrency === 'END') {
-                       return rl.close()
-                    }
-
-                    targetCurrency = selectedTargetCurrency.toUpperCase()
-                    console.log("target currency:" + selectedTargetCurrency)
-
-                    const url = `https://api.fastforex.io/historical?date=${givenDate}&from=${baseCurrency}&api_key=${apiKey}`
-                    fetch(url)
-                    .then((res) => res.json())
-                    .then((data) => {
-                        let currentExchangeRate = Number(data.results[targetCurrency])
-                        let calculation = amount * currentExchangeRate
-                        let result = `${amount} ${baseCurrency} is ${calculation.toFixed(2)} ${targetCurrency}`
-                        console.log(result)
-                        let toJson = {result: result}
-
-                        fs.readFile("conversions.json", (err, data) => {
-                            let json = [];
-                            if (!err && data.length > 0) {
-                                try {
-                                    json = JSON.parse(data);
-                                } catch (e) {
-                                    console.log("Error parsing JSON, starting with an empty array");
-                                }
+                            if(selectedTargetCurrency === 'END') {
+                               return rl.close()
                             }
-                            json.push(toJson);
-                            fs.writeFile("conversions.json", JSON.stringify(json, null, 2), (err) => {
-                                if (err) {
-                                    console.log(err);
+        
+                            targetCurrency = selectedTargetCurrency.toUpperCase()
+                            console.log("target currency:" + selectedTargetCurrency)
+                            //validation
+                            fetch(urlCurrencies)
+                            .then((res) => res.json())
+                            .then((data) => {
+                                let validCurrencies = data.currencies
+                                if(targetCurrency in validCurrencies) {
+                                    const url = `https://api.fastforex.io/historical?date=${givenDate}&from=${baseCurrency}&api_key=${apiKey}`
+                                    fetch(url)
+                                    .then((res) => res.json())
+                                    .then((data) => {
+                                        let currentExchangeRate = Number(data.results[targetCurrency])
+                                        let calculation = amount * currentExchangeRate
+                                        let result = `${amount} ${baseCurrency} is ${calculation.toFixed(2)} ${targetCurrency}`
+                                        console.log(result)
+                                        let toJson = {result: result}
+                
+                                        fs.readFile("conversions.json", (err, data) => {
+                                            let json = [];
+                                            if (!err && data.length > 0) {
+                                                try {
+                                                    json = JSON.parse(data);
+                                                } catch (e) {
+                                                    console.log("Error parsing JSON, starting with an empty array");
+                                                }
+                                            }
+                                            json.push(toJson);
+                                            fs.writeFile("conversions.json", JSON.stringify(json, null, 2), (err) => {
+                                                if (err) {
+                                                    console.log(err);
+                                                }
+                                            })
+                                        });
+                
+                                    }) 
+                
+                                    enterData()
+                                }
+                                else {
+                                    console.log("Please enter a valid currency code")
+                                    enterData()
                                 }
                             })
-                        });
+        
+                            
+                            });
+                    }
+                    else {
+                        console.log("Please enter a valid currency code")
+                        enterData()
+                    }
+                })
 
-                    }) 
+                
 
-                    enterData()
-                    });
+                
             })
                     
         })
